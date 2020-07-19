635423e3440f825824039fb1f7712c77e9d3cb4b
Rafael Martins <rafael.martins92@outlook.com>
2020-07-19 02:15:19 -0300

    Working with images on Gatsby

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
+import Avatar from "../Avatar"
+
 // import { Container } from './styles';
 
 const Profile = () => {
@@ -22,6 +24,7 @@ const Profile = () => {
 
   return (
     <div className="Profile-wrapper">
+      <Avatar />
       <h1>{title}</h1>
       <h2>{position}</h2>
       <p>{description}</p>
