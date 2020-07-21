726cbeeca5c772bbed36d06434adab9ba29f2db7
Rafael Martins <rafael.martins92@outlook.com>
2020-07-20 22:16:37 -0300

    Adjusting Post Item

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,18 +1,40 @@
 import React from "react"
+import PropTypes from "prop-types"
 
 import * as S from "./styled"
+import { SignLanguage } from "styled-icons/fa-solid"
 
-const PostItem = () => (
-  <S.PostItemLink to="/slug">
+const PostItem = ({
+  slug,
+  background,
+  category,
+  date,
+  timeToRead,
+  title,
+  description,
+}) => (
+  <S.PostItemLink to={slug}>
     <S.PostItemWrapper>
-      <S.PostItemTag background="#ff9999">Misc</S.PostItemTag>
+      <S.PostItemTag background={background}>{category}</S.PostItemTag>
       <S.PostItemInfo>
-        <S.PostItemDate>30 de Julho de 2020 - 4 min de leitura</S.PostItemDate>
-        <S.PostItemTitle>Vamos aprender JS</S.PostItemTitle>
-        <S.PostItemDescription>Dicas para aprender JS</S.PostItemDescription>
+        <S.PostItemDate>
+          {date} - {timeToRead} min de leitura
+        </S.PostItemDate>
+        <S.PostItemTitle>{title}</S.PostItemTitle>
+        <S.PostItemDescription>{description}</S.PostItemDescription>
       </S.PostItemInfo>
     </S.PostItemWrapper>
   </S.PostItemLink>
 )
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  background: PropTypes.string,
+  category: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+}
 
 export default PostItem
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,15 @@ import PostItem from "../components/PostItem"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <PostItem
+      slug="/about"
+      background="red"
+      category="Misc"
+      date="20 de Julho 2020"
+      timeToRead="5"
+      title="Vamos aprender JS"
+      description="Dicas para aprender JS"
+    />
   </Layout>
 )
 
